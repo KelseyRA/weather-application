c34fab40aec24a82d8c52097d603d1245514db7e
Luna Rae <kra7888@gmail.com>
2023-01-07 18:34:04 -0500

    Updated css styles and JS code.

--- assets/script.js
+++ assets/script.js
@@ -1,28 +1,101 @@
-var city = document.querySelector("#input");
-var submitBtn = document.querySelector("form");
-var cityName = document.querySelector(".card-title");
-var weatherData = document.querySelector(".card-text");
-
 // API Key
 const apiKey = '922f0ee8e01a77107a8791e91f1be21a';
 
-// var queryURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + apiKey +  "&units=imperial&cnt=40";
-var queryURL = "http://api.openweathermap.org/data/2.5/forecast?q=philadelphia&appid=" + apiKey + "&units=imperial&cnt=40";
+var cityInput = document.querySelector("#city-input");
+var cityName = document.querySelector(".city-name");
+var submitBtn = document.querySelector("#submit-btn");
+var cityName = document.querySelector(".card-title");
+var weatherData = document.querySelector(".card-text");
+var weatherHistory = document.querySelector(".li-history")
 
 
-submitBtn.addEventListener('click' , function(event) {
-    event.preventDefault()
-})
 
-function getApi() {
+// When the submit button is clicked the API is called
 
+function formSubmitHandler(event) {
+    event.preventDefault();
+    
+    var location = cityInput.value;
+    
+
+    if (location) {
+        getApi(location);
+
+        cityInput.textContent = '';
+
+    }
+
+    renderSearchHistory();
+    renderCurrentWeather();
+}
+
+function renderCurrentWeather() {
+    cityName.textContent = cityInput.value;
+}
+
+function renderSearchHistory() {
+    weatherHistory.textContent = cityInput.value;
+
+}
+
+// Function calls the API information and displays it in the console.
+function getApi(location) {
+
+ var queryURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + location + "&appid=" + apiKey +  "&units=imperial&cnt=40";
+    
     fetch(queryURL)
         .then(function (response) {
             return response.json();
-        })
-            .then(function (data) {
-             console.log(data)
-             })
+       })
+             .then(function (data) {
+                console.log(data)
+                })
+             
 }
 
-getApi()
+// function getApi(location) {
+//     var queryURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + location + "&appid=" + apiKey +  "&units=imperial&cnt=40";
+        
+//     fetch(queryURL)
+//      .then(function (response) {
+//         if(response.ok) {
+//         console.log(response);
+//         response.json().then(function (data) {
+//             console.log(data);
+//             displayWeather(data, temp_min , temp_max ,description)
+//         });
+//     } else {
+//         alert('Invalid Entry')
+//     }
+//     })
+            
+// }
+
+function displayWeather(data, temp_min , temp_max ,description) {
+    console.log(data)
+    console.log(temp_min)
+    console.log(temp_max)
+    console.log(description)
+
+}
+
+
+// Function gathers user input
+
+function userInfo() {
+    
+}
+
+
+// Function displays the searched city's most recent weather information at the top of the page.
+
+
+// Function displays the 5-day forecast below the most recent weather information.
+
+
+
+// Function retains the history of the search at the bottom of the page.
+
+//  getApi()
+
+submitBtn.addEventListener('click' , formSubmitHandler);
